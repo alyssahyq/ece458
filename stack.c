5a16cfefea73d0221ced21970506c3c5bb4dc908
alyssahyq <alyssahyq@outlook.com>
2022-05-10 11:45:19 -0400

    c file

---
+++ stack.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Changing this size will change the layout of the stack. */
+#ifndef BUF_SIZE
+#define BUF_SIZE 100
+#endif
+
+int bof(char * str)
+{
+	char buffer[BUF_SIZE];
+
+	/*The following statement has a buffer overflow problem */
+	strcpy(buffer, str);
+
+	return 1;
+}
+
+int main(int argc, char ** argv)
+{
+	char str[517];
+	FILE *badfile;
+
+	badfile = fopen("badfile", "r");
+	fread(str, sizeof(char), 517, badfile);
+	bof(str);
+	printf("Returned Properly\n");
+	return 1;
+}
